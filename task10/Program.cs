cbd2b3586457f7d55b49e615ac0a5ef3dfd04e1c
Natalia Frolova <n.smirnova76@mail.ru>
2022-08-25 12:30:04 +0300

    editing

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,5 +1,7 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int numberA = new Random().Next(100,999);
+
+int numberA = new Random().Next(100, 999);
+
 Console.WriteLine($"Three-digit number: {numberA}");
 
 Console.WriteLine($"Second digit: {numberA / 10 % 10}");
